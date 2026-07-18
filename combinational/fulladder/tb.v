57905bb86e2babcb080391bd1fd2a9ee676535cb
p . venkata vamsi <vamsipayasampalli24@gmail.com>
2026-07-18 11:21:08 +0530

    Add files via upload

---
+++ combinational/fulladder/tb.v
@@ -0,0 +1,16 @@
+module tb;
+reg a,b,cin;
+wire sum,cout;
+integer i;
+full_adder dut(a,b,cin,sum,cout);
+initial begin
+for(i=0;i<8;i=i+1) begin
+{a,b,cin}=i; 
+#10;
+$display("time=%0t,a=%b,b=%b,cin=%b,sum=%b,cout=%b",$time,a,b,cin,sum,cout);
+end
+#10 $finish;
+end
+endmodule
+
+
