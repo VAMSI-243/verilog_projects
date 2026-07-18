eeb221088d1412f9f9a37a26fdc871601dad8dba
p . venkata vamsi <vamsipayasampalli24@gmail.com>
2026-07-18 10:38:03 +0530

    Add files via upload

---
+++ combinational/1_bitcomparator/tb.v
@@ -0,0 +1,12 @@
+module tb;
+reg a,b;
+wire l,e,g;
+integer i;
+bit_1com dut(a,b,l,e,g);
+initial begin
+for(i=0;i<4;i=i+1)begin
+{a,b}=i;#10;
+$display("time=%0t,a=%b,b=%b,l=%b,e=%b,g=%b",$time,a,b,l,e,g);
+end
+end
+endmodule
