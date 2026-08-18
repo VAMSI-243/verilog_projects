b3268b595b0188b57b814e1520d582f766311cfe
p . venkata vamsi <vamsipayasampalli24@gmail.com>
2026-08-18 19:16:43 +0530

    Add files via upload

--- sequential/COUNTERS/ASY COUNTER/asy_counter.v
+++ sequential/COUNTERS/ASY COUNTER/asy_counter.v
@@ -1,3 +1,4 @@
+//ASY UPCOUNTER
 module asy_counter(clk,rst,count);
 parameter WIDTH=3;
 input clk,rst;
@@ -8,13 +9,14 @@ always@(posedge clk  or posedge rst) begin
 	else
 		count[0]<=~count[0];
 end
-always@(posedge ~count[0] or  posedge rst) begin
+always@(posedge ~count[0] or  posedge rst) begin   //COUNT[0] FOR DOWN COUNTER
 	if(rst)
 		count[1]<=0;
 	else
 		count[1]<=~count[1];
 end
-always@(posedge ~count[1] or posedge rst) begin
+always@(posedge ~count[1] or posedge rst) begin     //COUNT[1] FOR DOWN COUNTER
+
 	if(rst)
 		count[2]<=0;
 	else
