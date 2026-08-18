9198c3c4366ac3598ba1dabd57774d1788adc12d
p . venkata vamsi <vamsipayasampalli24@gmail.com>
2026-08-18 19:14:40 +0530

    Add files via upload

---
+++ sequential/COUNTERS/ASY COUNTER/asy_counter.v
@@ -0,0 +1,23 @@
+module asy_counter(clk,rst,count);
+parameter WIDTH=3;
+input clk,rst;
+output  reg[WIDTH-1:0] count;
+always@(posedge clk  or posedge rst) begin
+	if(rst)
+		count[0]<=0;
+	else
+		count[0]<=~count[0];
+end
+always@(posedge ~count[0] or  posedge rst) begin
+	if(rst)
+		count[1]<=0;
+	else
+		count[1]<=~count[1];
+end
+always@(posedge ~count[1] or posedge rst) begin
+	if(rst)
+		count[2]<=0;
+	else
+		count[2]<=~count[2];
+end
+endmodule
---
+++ sequential/COUNTERS/ASY COUNTER/tb.v
@@ -0,0 +1,22 @@
+`timescale 1ns/1ps
+module tb;
+parameter WIDTH=3;
+reg clk,rst;
+wire [WIDTH-1:0]count;
+asy_counter dut(clk,rst,count);
+initial begin
+	clk=0;
+	forever #5 clk=~clk;
+end
+always@(posedge clk)
+	 $strobe($time,"clk=%b,rst=%b,count=%b",clk,rst,count);
+initial begin
+	 rst=1;
+	#10 rst=0;
+	#100 rst=1;
+	#10 rst=0;
+	#100 $finish;
+end
+endmodule
+
+
