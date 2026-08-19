35963ea55d43ac42e22c713dae47fdd107d8e4bc
p . venkata vamsi <vamsipayasampalli24@gmail.com>
2026-08-19 17:00:57 +0530

    Update counter.v

--- sequential/COUNTERS/SYN COUNTER USING FF/counter.v
+++ sequential/COUNTERS/SYN COUNTER USING FF/counter.v
@@ -15,7 +15,7 @@ input clk,rst,m;
 output [WIDTH-1:0]count;
 wire [WIDTH-1:0]q;
 wire [WIDTH-1:0]d;
-assign d=(m==1)?q-3'd1:q+3'd1;  
+assign d=(m==1)?q-3'd1:q+3'd1;    //If m==1 it acts as upcounter else down counter
 dff u0(clk,rst,d[0],q[0]);
 dff u1(clk,rst,d[1],q[1]);
 dff u2(clk,rst,d[2],q[2]);
