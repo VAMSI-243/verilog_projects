130e9f64ee3e17f7c16ddcee1ded7505ab4610c0
p . venkata vamsi <vamsipayasampalli24@gmail.com>
2026-08-23 17:14:35 +0530

    Add files via upload

---
+++ sequential/COUNTERS/JOHNSON COUNTER/johnson_counter.v
@@ -0,0 +1,12 @@
+module johnson_counter(clk,rst,count);
+parameter WIDTH=3;
+input clk,rst;
+output reg [WIDTH-1:0]count;
+always@(posedge clk) begin
+if(rst)
+count<=3'b000;
+else
+count<={~count[0],count[2:1]};
+end
+endmodule
+
---
+++ sequential/COUNTERS/JOHNSON COUNTER/tb.v
@@ -0,0 +1,19 @@
+`timescale 1ns/1ps
+module tb;
+parameter WIDTH=3;
+reg clk,rst;
+wire [WIDTH-1:0]count;
+johnson_counter dut(clk,rst,count);
+initial begin
+clk=0;
+forever #5 clk=~clk;
+end
+initial begin
+rst=1; #10;
+rst=0;#70;
+rst=1;#10;
+rst=0;
+#50 $finish;
+end
+endmodule
+
